467b7bda53550c8fb0d41a8555d87a90f86d584d
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2024-08-24 12:08:15 +0700

    user win

--- models/users.js
+++ models/users.js
@@ -3,8 +3,15 @@ const schema=mongoose.Schema
 
 const modelUsers= new schema({
     name:{type:String},
+    phone:{type:String},
+    email:{type:String},
+    guestOf:{type:String},
+    isConfirm:{type:Boolean},
+    numberOfPeople:{type:Number},
+    desc:{type:String},
     userName:{type:String},
     passWord:{type:String},
+    role:{type: String, default: 'Member' }
 },
 { timestamps: true }
 )
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const user= require('../controllers/users')
+const verifyToken=require('../middlewaves/authenMiddeware')
+router.post('/create',verifyToken,user.CreateUsers)
+router.post('/update',verifyToken,user.UpdateUsers)
+router.delete('/delete/:_id',verifyToken,user.DeleteUser)
+router.get('/get',verifyToken,user.GetUsers)
 module.exports = router;

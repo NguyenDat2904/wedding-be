d9a6e565628b9da533960b1e43a41c9a8fd595bb
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2024-09-09 16:16:11 +0700

    fix

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,5 @@ router.post('/create',verifyToken,user.CreateUsers)
 router.post('/update/:_id',verifyToken,user.UpdateUsers)
 router.post('/sendEmail',verifyToken,user.SendEmailClient)
 router.delete('/delete/:_id',verifyToken,user.DeleteUser)
-router.get('/get',verifyToken,user.GetUsers)
+router.get('/get',user.GetUsers)
 module.exports = router;

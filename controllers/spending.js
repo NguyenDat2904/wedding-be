c7d32462329c56e4f83c5de3c548cf675e5f0f1e
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2024-09-20 14:34:24 +0700

    fix spending

--- controllers/spending.js
+++ controllers/spending.js
@@ -2,14 +2,14 @@ const modelSpending=require('../models/spendings')
 
 const CreateSpending=async(req,res)=>{
     try {
-        const {addSpending,amount,desc,status} =req.body
-        if(!addSpending||!amount||!desc||!status){
+        const {addSpending,amount,desc,status,money} =req.body
+        if(!addSpending||!amount||!desc||!status||!money){
             return res.status(300).json({
                 message:'Bạn vui lòng điền đầy đủ thông tin'
             })
         }
         const newSpending= await modelSpending.insertMany({
-            addSpending,amount,desc,status
+            addSpending,amount,desc,status,money
         })
         return res.status(200).json({
             message:'successfully',
@@ -24,14 +24,14 @@ const CreateSpending=async(req,res)=>{
 const UpdateSpending=async(req,res)=>{
     try {
         const {_id}=req.params
-        const {addSpending,amount,desc,status} =req.body
-        if(!_id||!addSpending||!amount||!status){
+        const {addSpending,amount,desc,status,money} =req.body
+        if(!_id||!addSpending||!amount||!status||!money){
             return res.status(300).json({
                 message:'Bạn vui lòng điền đầy đủ thông tin hoặc chưa có id'
             })
         }
         const update= await modelSpending.findByIdAndUpdate(_id,{
-            addSpending,amount,desc,status
+            addSpending,amount,desc,status,money
         },{new:true})
         return res.status(200).json({
             message:'success',
@@ -66,16 +66,27 @@ const GetSpending=async(req,res)=>{
         const status =req.query.status
         const limit = req.query.limit || 25
         const skip = req.query.skip || 1
-        const lengthSpending= await modelSpending.find(
-           {...( status !== undefined && {status:status})}
-        )
+        const search=req.query.search||''
+        const lengthSpending= await  modelSpending.aggregate([
+            {$match:{
+                $or:[
+                    {addSpending:{$regex:search,$options: 'i' }}
+                ],
+                ...( status !== undefined && {status:status})
+            }}
+        ])
         const totalPage= Math.ceil(lengthSpending.length / limit)
-        const dataSpending= await modelSpending.find(
-           {...( status !== undefined && {status:status})}
-        )
-        .sort({ createdAt: -1 })
-        .skip((skip-1)*limit)
-        .limit(limit)
+        const dataSpending= await modelSpending.aggregate([
+            {$match:{
+                $or:[
+                    {addSpending:{$regex:search,$options: 'i' }}
+                ],
+                ...( status !== undefined && {status:status})
+            }},
+            {$sort:{ createdAt: -1 }},
+            { $skip: ((skip - 1) * limit) },
+            { $limit: limit },
+        ])
         return res.status(200).json({
             data:dataSpending,
             totalPage:totalPage
